f8d909cae0aedca3d8576c697f07e1df013b8bdf
alonsogonzalezleal <alonsogonzalez423@gmail.com>
2023-05-27 01:15:14 -0600

    Se limpió el proyecto base de MAUI

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TheAsker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TheAsker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
